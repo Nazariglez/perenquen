943dde464060ae16f18984e60854ae0b8ec4396f
Nazariglez <nazari.nz@gmail.com>
2015-03-14 22:26:11 +0000

    Basic sprite

--- src/display/Sprite.js
+++ src/display/Sprite.js
@@ -1,4 +1,5 @@
-var Sprite = require('../../lib/pixi/src/core/sprites/Sprite'),
+var SpriteRenderer = require('./SpriteRenderer'),
+    Sprite = require('../../lib/pixi/src/core/sprites/Sprite'),
     Container = require('./Container'),
     utils = require('../core/utils'),
     math = require('../../lib/pixi/src/core/math'),
@@ -6,6 +7,80 @@ var Sprite = require('../../lib/pixi/src/core/sprites/Sprite'),
 
 utils.mixin(Sprite, mixin);
 
-//Sprite.prototype.displayObjectUpdateTransform = Container.prototype.displayObjectUpdateTransform;
+Sprite.prototype.displayObjectUpdateTransform = function(){
+    // create some matrix refs for easy access
+    var pt = this.parent.worldTransform;
+    var wt = this.worldTransform;
+
+    // temporary matrix variables
+    var a, b, c, d, tx, ty;
+
+    var anchorWidth = this.anchor.x * this.width,
+        anchorHeight = this.anchor.y * this.height,
+        pivotWidth = this.pivot.x * this.width,
+        pivotHeight = this.pivot.y * this.height;
+
+    // so if rotation is between 0 then we can simplify the multiplication process...
+    if (this.rotation % math.PI_2)
+    {
+        // check to see if the rotation is the same as the previous render. This means we only need to use sin and cos when rotation actually changes
+        if (this.rotation !== this.rotationCache)
+        {
+            this.rotationCache = this.rotation;
+            this._sr = Math.sin(this.rotation);
+            this._cr = Math.cos(this.rotation);
+        }
+
+        // get the matrix values of the displayobject based on its transform properties..
+        a  =  this._cr * this.scale.x;
+        b  =  this._sr * this.scale.x;
+        c  = -this._sr * this.scale.y;
+        d  =  this._cr * this.scale.y;
+        tx =  this.position.x - pivotWidth * this.scale.x + anchorWidth*this.scale.x;
+        ty =  this.position.y - pivotHeight * this.scale.y + anchorHeight*this.scale.y;
+
+        //TODO: EL Pivot no se comporta como debe, REVISAR
+        // check for pivot.. not often used so geared towards that fact!
+        if (pivotWidth !== anchorWidth) {
+            tx -= pivotWidth * a;
+        }
+
+        if (pivotHeight !== anchorHeight) {
+            ty -= pivotWidth * d;
+        }
+
+        // concat the parent matrix with the objects transform.
+        wt.a  = a  * pt.a + b  * pt.c;
+        wt.b  = a  * pt.b + b  * pt.d;
+        wt.c  = c  * pt.a + d  * pt.c;
+        wt.d  = c  * pt.b + d  * pt.d;
+        wt.tx = tx * pt.a + ty * pt.c + pt.tx;
+        wt.ty = tx * pt.b + ty * pt.d + pt.ty;
+    }
+    else
+    {
+        // lets do the fast version as we know there is no rotation..
+        a  = this.scale.x;
+        d  = this.scale.y;
+
+        tx = this.position.x;// + anchorWidth * a;
+        ty = this.position.y;// + anchorHeight * a;
+
+        wt.a  = a  * pt.a;
+        wt.b  = a  * pt.b;
+        wt.c  = d  * pt.c;
+        wt.d  = d  * pt.d;
+        wt.tx = tx * pt.a + ty * pt.c + pt.tx;
+        wt.ty = tx * pt.b + ty * pt.d + pt.ty;
+    }
+
+    // multiply the alphas..
+    this.worldAlpha = this.alpha * this.parent.worldAlpha;
+
+    // reset the bounds each time this is called!
+    this._currentBounds = null;
+};
+
+
 
 module.exports = Sprite;
