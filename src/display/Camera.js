09e0f12aebeb29cfa4661d105dc55db9f73913d8
Nazariglez <nazari.nz@gmail.com>
2015-03-17 19:03:57 +0000

    added camera module

---
+++ src/display/Camera.js
@@ -0,0 +1,65 @@
+var Container = require('./Container');
+
+function Camera(scene){
+    Container.call(this);
+    this.scene = scene;
+
+    this._zoom = 1;
+
+    this.blockX = false;
+    this.blockY = false;
+
+    this.setSize(scene.width, scene.height)
+        .setPosition(scene.width/2, scene.height/2);
+}
+
+Camera.prototype = Object.create(Container.prototype);
+Camera.prototype.constructor = Camera;
+
+Camera.prototype.setFollow = function(target){
+
+};
+
+Camera.prototype.unFollow = function(){
+    return this.setFollow(false);
+};
+
+Camera.prototype.setZoom = function(value){
+
+};
+
+Camera.prototype.zoomIn = function(value){
+
+};
+
+Camera.prototype.zoomOut = function(value){
+
+};
+
+Camera.prototype.goToPosition = function(x,y){
+
+};
+
+Camera.prototype.goToTarget = function(target){
+
+};
+
+Camera.prototype.setBlock = function(x,y){
+    this.blockX = !!x;
+    this.blockY = !!y;
+    return this;
+};
+
+Object.defineProperties(Camera.prototype, {
+    zoom: {
+        get: function(){
+            return this._zoom;
+        },
+        set: function(value){
+            //todo:
+            this._zoom = value;
+        }
+    }
+});
+
+module.exports = Camera;
