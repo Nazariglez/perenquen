8eda0c3a92595c0e5a85b8ffc54aad862c28c55e
Nazariglez <nazari.nz@gmail.com>
2015-03-20 20:53:01 +0000

    TilingSprite now use the sprite name like ID

--- src/display/TilingSprite.js
+++ src/display/TilingSprite.js
@@ -1,7 +1,16 @@
-var TilingSprite = require('../../lib/pixi/src/extras/TilingSprite'),
+var PixiTilingSprite = require('../../lib/pixi/src/extras/TilingSprite'),
     utils = require('../core/utils'),
     mixin = require('./mixin');
 
+function TilingSprite(texture, width, height){
+    if(typeof texture === "string"){
+        texture = utils.assetCache.getTexture(texture);
+    }
+    PixiTilingSprite.call(this, texture, width, height);
+}
+
+TilingSprite.prototype = Object.create(PixiTilingSprite.prototype);
+TilingSprite.prototype.constructor = TilingSprite;
 utils.mixin(TilingSprite, mixin);
 
 //TODO: Funciona fatal, recortado la textura, situandola mal, no respetando la escala, quizás sea cosa de la v3 de pixi?
