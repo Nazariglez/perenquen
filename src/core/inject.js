428ed1e8ab84ad99e5acb2b9edef2bf3daee243e
Nazariglez <nazari.nz@gmail.com>
2015-04-04 15:32:20 +0100

    Improved plugin system, now we can use this._super() in the injection methods

--- src/core/inject.js
+++ src/core/inject.js
@@ -14,11 +14,33 @@ function inject(className, fn){
     var proto = fn.call(this,baseClassProto),
         ctor = null;
 
+    var isSuper = !!baseClass._super;
+    var inheritanceFn = function (name, fn) {
+        return function () {
+            var tmp = this._super;
+            this._super = baseClass._super[name];
+            var ret = fn.apply(this, arguments);
+            this._super = tmp;
+            return ret;
+        };
+    };
+
     for(var pr in proto){
         if (proto.hasOwnProperty(pr)) {
-            if(typeof proto[pr] === "function" && pr === ctorName){
-                ctor = proto[pr];
-                newProto.constructor = proto[pr];
+            if(typeof proto[pr] === "function") {
+                if(isSuper && baseClass._super[pr] && typeof baseClass._super[pr] === "function"){
+                    proto[pr] = inheritanceFn(pr, proto[pr]);
+                    if (pr === ctorName && baseClass._super["constructor"]) {
+                        proto[pr] = inheritanceFn("constructor", proto[pr]);
+                    }else{
+                        proto[pr] = inheritanceFn(pr, proto[pr]);
+                    }
+                }
+
+                if (pr === ctorName) {
+                    ctor = proto[pr];
+                    newProto.constructor = proto[pr];
+                }
             }
 
             newProto[pr] = proto[pr];
@@ -26,9 +48,17 @@ function inject(className, fn){
     }
 
     if(ctor){
-        baseClass = function PQClass(){
-            ctor.apply(this, arguments);
-        };
+        if(isSuper){
+            var sup = Object.create(baseClass._super);
+            baseClass = function PQClass(){
+                ctor.apply(this, arguments);
+            };
+            baseClass._super = sup;
+        }else{
+            baseClass = function PQClass(){
+                ctor.apply(this, arguments);
+            };
+        }
     }
 
     baseClass.prototype = newProto;
