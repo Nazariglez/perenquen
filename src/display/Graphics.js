3365520d6239cddd7346dec1ffa5bb537d470e37
Nazariglez <nazari.nz@gmail.com>
2015-04-07 19:20:27 +0100

    graphics can draw paths now

--- src/display/Graphics.js
+++ src/display/Graphics.js
@@ -4,7 +4,9 @@ var Graphics = require('../../lib/pixi/src/core/graphics/Graphics'),
 
 utils.mixin(Graphics, mixin);
 
-
-
+Graphics.prototype.drawPath = function(path){
+    this.drawShape(path.polygon);
+    return this;
+};
 
 module.exports = Graphics;
--- src/extra/Path.js
+++ src/extra/Path.js
@@ -4,6 +4,8 @@ var Graphics = require('../display/Graphics'),
 function Path(){
     this.curveAccuracy = 30;
     this.polygon = new math.Polygon();
+
+    this.tmpPoint = new math.Point();
 }
 
 Path.prototype.constructor = Path;
@@ -24,4 +26,37 @@ Path.prototype.moveTo = function(x,y){
     return this;
 };
 
+Path.prototype.lineTo = function(x,y){
+    if(this.polygon.points.length === 0){
+        this.moveTo(0,0);
+    }
+
+    this.polygon.points.push(x,y);
+    return this;
+};
+
+Path.prototype.getPoint = function(num){
+    var len = num*2;
+    this.tmpPoint.set(this.polygon.points[len],this.polygon.points[len + 1]);
+    return this.tmpPoint;
+};
+
+Path.prototype.close = function(){
+    var start = this.getPoint(0);
+    this.lineTo(start.x, start.y);
+    return this;
+};
+
+Path.prototype.generateGraphics = function(){
+    //TODO: generateGraphics
+};
+
 module.exports = Path;
+
+Object.defineProperties(Path.prototype, {
+    length : {
+        get : function(){
+            return (this.polygon.points.length === 0) ? 0 : this.polygon.points.length/2;
+        }
+    }
+});
