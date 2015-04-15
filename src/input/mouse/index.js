2b02d0bfa7a5df020ddd17f59965f261da861559
Nazariglez <nazari.nz@gmail.com>
2015-04-15 01:07:15 +0100

    minor update

--- src/input/mouse/index.js
+++ src/input/mouse/index.js
@@ -3,3 +3,7 @@ var interaction = {
         InteractionManager: require('./InteractionManager'),
         interactiveTarget: require('../../../lib/pixi/src/interaction/interactiveTarget')
     };
+
+//TODO: Reescribir el sistema de interactividad, añadiendo eventos como drag y doble click, gestion de rueda de ratón, nuevo eventdata, multitouch, pixelperfect, keyhandler con combinación de teclas, etc...
+
+module.exports = interaction;
