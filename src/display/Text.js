25cc498e885e44bf00e7699459038c5d0a35d532
Nazariglez <nazari.nz@gmail.com>
2015-05-01 22:34:20 +0100

    fixed some anchor issues in PQ.Text, also added properties like pivot or depth

--- src/display/Text.js
+++ src/display/Text.js
@@ -15,15 +15,248 @@ Text.fontPropertiesCache = {};
 Text.fontPropertiesCanvas = document.createElement('canvas');
 Text.fontPropertiesContext = Text.fontPropertiesCanvas.getContext('2d');
 
-//TODO: wrong displayObjectTransform, need a custom transform
-
 Text.prototype._init = function(text, style, resolution){
     text = text || ' ';
     PixiText.call(this, text, style, resolution);
 
     this.speed = new math.Point();
     this.anchor = new math.Point(0.5, 0.5);
+    this.pivot = new math.Point(0.5, 0.5);
 };
+
+Text.prototype.displayObjectUpdateTransform = function(){
+    // create some matrix refs for easy access
+    var pt = this.parent.worldTransform;
+    var wt = this.worldTransform;
+
+    // temporary matrix variables
+    var a, b, c, d, tx, ty;
+
+    var anchorWidth = this.anchor.x * this._width * this.scale.x,
+        anchorHeight = this.anchor.y * this._height * this.scale.y,
+        pivotWidth = this.pivot.x * this._width * this.scale.x,
+        pivotHeight = this.pivot.y * this._height * this.scale.y;
+
+    // so if rotation is between 0 then we can simplify the multiplication process...
+    if (this.rotation % CONST.PI_2)
+    {
+        // check to see if the rotation is the same as the previous render. This means we only need to use sin and cos when rotation actually changes
+        if (this.rotation !== this.rotationCache)
+        {
+            this.rotationCache = this.rotation;
+            this._sr = Math.sin(this.rotation);
+            this._cr = Math.cos(this.rotation);
+        }
+
+        // get the matrix values of the displayobject based on its transform properties..
+        a  =  this._cr * this.scale.x;
+        b  =  this._sr * this.scale.x;
+        c  = -this._sr * this.scale.y;
+        d  =  this._cr * this.scale.y;
+        tx =  this.position.x + pivotWidth - anchorWidth;
+        ty =  this.position.y + pivotHeight - anchorHeight;
+
+        if (pivotWidth || pivotHeight)
+        {
+            tx -= pivotWidth * this._cr + pivotHeight * -this._sr;
+            ty -= pivotWidth * this._sr + pivotHeight * this._cr;
+        }
+
+        // concat the parent matrix with the objects transform.
+        wt.a  = a  * pt.a + b  * pt.c;
+        wt.b  = a  * pt.b + b  * pt.d;
+        wt.c  = c  * pt.a + d  * pt.c;
+        wt.d  = c  * pt.b + d  * pt.d;
+        wt.tx = tx * pt.a + ty * pt.c + pt.tx;
+        wt.ty = tx * pt.b + ty * pt.d + pt.ty;
+    }
+    else
+    {
+        // lets do the fast version as we know there is no rotation..
+        a  = this.scale.x;
+        d  = this.scale.y;
+
+        tx = this.position.x - anchorWidth;
+        ty = this.position.y - anchorHeight;
+
+        wt.a  = a  * pt.a;
+        wt.b  = a  * pt.b;
+        wt.c  = d  * pt.c;
+        wt.d  = d  * pt.d;
+        wt.tx = tx * pt.a + ty * pt.c + pt.tx;
+        wt.ty = tx * pt.b + ty * pt.d + pt.ty;
+    }
+
+    // multiply the alphas..
+    this.worldAlpha = this.alpha * this.parent.worldAlpha;
+
+    // reset the bounds each time this is called!
+    this._currentBounds = null;
+};
+
+Text.prototype._renderCanvas = function (renderer)
+{
+    if (this.dirty)
+    {
+        //   this.resolution = 1//renderer.resolution;
+
+        this.updateText();
+    }
+
+    //Sprite.prototype._renderCanvas.call(this, renderer);
+    this._customRenderCanvas(renderer);
+};
+
+Text.prototype._customRenderCanvas = function(renderer){
+    if (this.texture.crop.width <= 0 || this.texture.crop.height <= 0)
+    {
+        return;
+    }
+
+    if (this.blendMode !== renderer.currentBlendMode)
+    {
+        renderer.currentBlendMode = this.blendMode;
+        renderer.context.globalCompositeOperation = renderer.blendModes[renderer.currentBlendMode];
+    }
+
+    //  Ignore null sources
+    if (this.texture.valid)
+    {
+        var texture = this._texture,
+            wt = this.worldTransform,
+            dx,
+            dy,
+            width,
+            height;
+
+        var resolution = texture.baseTexture.resolution / renderer.resolution;
+
+        renderer.context.globalAlpha = this.worldAlpha;
+
+        // If smoothingEnabled is supported and we need to change the smoothing property for this texture
+        if (renderer.smoothProperty && renderer.currentScaleMode !== texture.baseTexture.scaleMode)
+        {
+            renderer.currentScaleMode = texture.baseTexture.scaleMode;
+            renderer.context[renderer.smoothProperty] = (renderer.currentScaleMode === CONST.SCALE_MODES.LINEAR);
+        }
+
+        // If the texture is trimmed we offset by the trim x/y, otherwise we use the frame dimensions
+
+        if(texture.rotate)
+        {
+
+            // cheeky rotation!
+            var a = wt.a;
+            var b = wt.b;
+
+            wt.a  = -wt.c;
+            wt.b  = -wt.d;
+            wt.c  =  a;
+            wt.d  =  b;
+
+            width = texture.crop.height; //TODO: Width assigned to height???
+            height = texture.crop.width;
+
+            dx = (texture.trim) ? texture.trim.y - this.anchor.y * texture.trim.height : this.anchor.y * -texture._frame.height;
+            dy = (texture.trim) ? texture.trim.x - this.anchor.x * texture.trim.width : this.anchor.x * -texture._frame.width;
+        }
+        else
+        {
+            width = texture.crop.width;
+            height = texture.crop.height;
+
+            dx = (texture.trim) ? texture.trim.x - this.anchor.x * texture.trim.width : this.anchor.x * -texture._frame.width;
+            dy = (texture.trim) ? texture.trim.y - this.anchor.y * texture.trim.height : this.anchor.y * -texture._frame.height;
+        }
+
+
+
+        // Allow for pixel rounding
+        if (renderer.roundPixels)
+        {
+            renderer.context.setTransform(
+                wt.a,
+                wt.b,
+                wt.c,
+                wt.d,
+                (wt.tx * renderer.resolution) | 0,
+                (wt.ty * renderer.resolution) | 0
+            );
+
+            dx = dx | 0;
+            dy = dy | 0;
+        }
+        else
+        {
+
+            renderer.context.setTransform(
+                wt.a,
+                wt.b,
+                wt.c,
+                wt.d,
+                wt.tx * renderer.resolution,
+                wt.ty * renderer.resolution
+            );
+
+
+        }
+
+        var anchorWidth = this.anchor.x * this._width/resolution,
+            anchorHeight = this.anchor.y * this._height/resolution;
+
+        if (this.tint !== 0xFFFFFF)
+        {
+            if (this.cachedTint !== this.tint)
+            {
+                this.cachedTint = this.tint;
+
+                // TODO clean up caching - how to clean up the caches?
+                // TODO: dont works with spritesheets
+                this.tintedTexture = CanvasTinter.getTintedTexture(this, this.tint);
+            }
+
+            renderer.context.drawImage(
+                this.tintedTexture,
+                0,
+                0,
+                width * resolution * renderer.resolution,
+                height * resolution * renderer.resolution,
+                dx / resolution,
+                dy / resolution,
+                width * renderer.resolution,
+                height * renderer.resolution
+            );
+        }
+        else
+        {
+            //TODO: cuando la resolución del renderer es mayor a 1 los sprites se muestran mal
+            renderer.context.drawImage(
+                texture.baseTexture.source,
+                texture.crop.x * resolution,
+                texture.crop.y * resolution,
+                width * resolution * renderer.resolution,
+                height * resolution * renderer.resolution,
+                dx / resolution + anchorWidth,
+                dy / resolution + anchorHeight,
+                width * renderer.resolution,
+                height * renderer.resolution
+            );
+        }
+    }
+};
+
+Text.prototype.renderWebGL = function (renderer)
+{
+    if (this.dirty)
+    {
+        //this.resolution = 1//renderer.resolution;
+
+        this.updateText();
+    }
+
+    Sprite.prototype.renderWebGL.call(this, renderer);
+};
+
 
 Text.prototype.updateText = function (){
     var style = this._style;
@@ -170,8 +403,9 @@ Text.prototype.setWordWrap = function(value){
     return this;
 };
 
-//TODO: Pivot, anchor...
-//TODO: depth??
+Text.prototype.containsPoint = Sprite.prototype.containsPoint;
+Text.prototype.getLocalBounds = Sprite.prototype.getLocalBounds;
+
 module.exports = Text;
 
 
