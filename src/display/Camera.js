716e855e5ff9f96e542249d0a9d21b1b43da122f
Nazariglez <nazari.nz@gmail.com>
2015-05-13 18:30:09 +0100

    added bound limits to the camera

--- src/display/Camera.js
+++ src/display/Camera.js
@@ -1,8 +1,11 @@
 var Container = require('./Container'),
     config = require('../core/config'),
     math = require('../../lib/pixi/src/core/math'),
-    tempPoint = new math.Point();
+    tempPoint = new math.Point(),
+    minPoint = new math.Point(),
+    maxPoint = new math.Point();
 
+//TODO: Separar camara de escena dejando que se implemente en cualquier container
 function Camera(scene){
     this._init(scene);
 }
@@ -19,15 +22,75 @@ Camera.prototype._init = function(scene){
     this.scene = scene;
 
     this._zoom = 1;
-    this.target = null;
+    this._target = null;
+    this.targetOffset = new math.Point();
     this.followType = Camera.followTypes.fixed;
     this.followVelocity = 0;
+
+    this._minX = false;
+    this._maxX = false;
+    this._minY = false;
+    this._maxY = false;
+
+    this._dirtyBounds = false;
+
+    this.minXBound = false;
+    this.maxXBound = false;
+    this.minYBound = false;
+    this.maxYBound = false;
 
     this.blockX = false;
     this.blockY = false;
 
     this.setSize(scene.width, scene.height)
         .setPosition(scene.width/2, scene.height/2);
+};
+
+Camera.prototype.checkLimits = function(){
+    if(this._dirtyBounds){
+        this._dirtyBounds = false;
+
+        //console.log('cheking bounds');
+        if(this.target){
+
+            var parentMatrix = this.target.parent.worldTransform,
+                minX = (this.minX !== false),
+                minY = (this.minY !== false),
+                maxX = (this.maxX !== false),
+                maxY = (this.maxY !== false);
+
+            if(minX || minY){
+                minPoint.x = (minX) ? this.minX : 0;
+                minPoint.y = (minY) ? this.minY : 0;
+
+                this.worldTransform.applyInverse(parentMatrix.apply(minPoint, minPoint));
+
+                if(minX)this.minXBound = -minPoint.x + this.x;
+                if(minY)this.minYBound = -minPoint.y + this.y;
+            }
+
+            if(maxX || maxY){
+                maxPoint.x = (maxX) ? this.maxX : 0;
+                maxPoint.y = (maxY) ? this.maxY : 0;
+
+                this.worldTransform.applyInverse(parentMatrix.apply(maxPoint, maxPoint));
+
+                if(maxX)this.maxXBound = -maxPoint.x + this.x + this.width;
+                if(maxY)this.maxYBound = -maxPoint.y + this.y + this.height;
+            }
+
+        }
+
+        //console.log(this.minXBound, this.minYBound, this.maxXBound, this.maxYBound);
+
+    }
+
+    return this;
+};
+
+Camera.prototype.setTargetOffset = function(x,y){
+    this.targetOffset.set(x,y);
+    return this;
 };
 
 Camera.prototype.setFixedTarget = function(target){
@@ -37,7 +100,15 @@ Camera.prototype.setFixedTarget = function(target){
 };
 
 Camera.prototype.setTarget = function(target){
-    this.target = (!target) ? null : target;
+    this.target = target;
+    return this;
+};
+
+Camera.prototype.setLimits = function(minX, minY, maxX, maxY){
+    this.minX = minX;
+    this.maxX = maxX;
+    this.minY = minY;
+    this.maxY = maxY;
     return this;
 };
 
@@ -45,6 +116,13 @@ Camera.prototype.animate = function(gameTime, delta){
     if(this.update(gameTime, delta) !== false){
 
         var tick = (config.useDeltaAnimation) ? delta : 1;
+
+        var len = this.children.length;
+        for(var i = 0; i < len; i++){
+            this.children[i].animate(gameTime, delta);
+        }
+
+        this.applyFollowTarget();
 
         if(this.speed && (this.speed.x !== 0 || this.speed.y !== 0)){
             this.position.x += this.speed.x * tick;
@@ -54,18 +132,12 @@ Camera.prototype.animate = function(gameTime, delta){
         if(this.rotationSpeed && this.rotationSpeed !== 0){
             this.rotation += this.rotationSpeed * tick;
         }
-
-        var len = this.children.length;
-        for(var i = 0; i < len; i++){
-            this.children[i].animate(gameTime, delta);
-        }
-
-        this.applyFollowTarget();
     }
 };
 
 Camera.prototype.applyFollowTarget = function(){
     if(this.target){
+        this.checkLimits();
         this.goToTarget(this.target);
     }
 };
@@ -88,19 +160,47 @@ Camera.prototype.goToPosition = function(x,y){
 };
 
 Camera.prototype.goToTarget = function(target){
-    var parentMatrix = target.parent.worldTransform;
-    parentMatrix.apply(target.position, tempPoint);
+    var x, y,
+        parentMatrix = target.parent.worldTransform;
 
-    var x = -tempPoint.x + this.x + this.width/ 2,
+    tempPoint.copy(target.position);
+    tempPoint.x += this.targetOffset.x;
+    tempPoint.y += this.targetOffset.y;
+
+    this.worldTransform.applyInverse(parentMatrix.apply(tempPoint, tempPoint));
+    if(!this.blockX){
+        x = -tempPoint.x + this.x + this.width/2;
+
+        if(this.minX !== false && x > this.minXBound){
+            x = this.minXBound;
+        }else if(this.maxX !== false && x < this.maxXBound){
+            x = this.maxXBound;
+        }
+
+        this.x = x;
+    }
+
+    if(!this.blockY){
         y = -tempPoint.y + this.y + this.height/2;
+        if(this.minY !== false && y > this.minYBound){
+            y = this.minYBound;
+        }else if(this.maxY !== false && y < this.maxYBound){
+            y = this.maxYBound;
+        }
 
-    this.position.set(x,y);
+        this.y = y;
+    }
+
     return this;
 };
 
-Camera.prototype.setBlock = function(x,y){
-    this.blockX = !!x;
-    this.blockY = !!y;
+Camera.prototype.setBlockX = function(value){
+    this.blockX = (value !== false);
+    return this;
+};
+
+Camera.prototype.setBlockY = function(value){
+    this.blockY = (value !== false);
     return this;
 };
 
@@ -112,6 +212,71 @@ Object.defineProperties(Camera.prototype, {
         set: function(value){
             //todo:
             this._zoom = value;
+        }
+    },
+
+    target: {
+        get: function(){
+            return this._target;
+        },
+
+        set: function(value){
+            if(!value){
+                this._target = null;
+                this.targetOffset.set(0,0);
+            }else{
+                this._target = value;
+            }
+
+            this._dirtyBounds = true;
+        }
+    },
+
+    minX : {
+        get: function(){
+            return this._minX;
+        },
+        set: function(value){
+            if(typeof value !== "number")value = false;
+
+            this._minX = value;
+            this._dirtyBounds = true;
+        }
+    },
+
+    maxX : {
+        get: function(){
+            return this._maxX;
+        },
+        set: function(value){
+            if(typeof value !== "number")value = false;
+
+            this._maxX = value;
+            this._dirtyBounds = true;
+        }
+    },
+
+    minY : {
+        get: function(){
+            return this._minY;
+        },
+        set: function(value){
+            if(typeof value !== "number")value = false;
+
+            this._minY = value;
+            this._dirtyBounds = true;
+        }
+    },
+
+    maxY : {
+        get: function(){
+            return this._maxY;
+        },
+        set: function(value){
+            if(typeof value !== "number")value = false;
+
+            this._maxY = value;
+            this._dirtyBounds = true;
         }
     }
 });
