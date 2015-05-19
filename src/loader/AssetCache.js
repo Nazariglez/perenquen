5cf438582d675b86c4c62b35c287239110d3c6c6
Nazariglez <nazari.nz@gmail.com>
2015-05-19 21:01:06 +0100

    improved assetcache methods

--- src/loader/AssetCache.js
+++ src/loader/AssetCache.js
@@ -12,27 +12,46 @@ AssetCache.prototype._init = function(){
 };
 
 AssetCache.prototype.addTexture = function(name, url, texture){
-    name = name || url;
-    this.textures[name] = {
-        url : url,
-        asset : texture
-    };
-    return this;
+    return this._addAssetIn(this.textures, name, url, texture);
 };
 
 AssetCache.prototype.getTexture = function(name){
-    var texture = this.textures[name];
+    return this._getAssetFrom(this.textures, name);
+};
 
-    if(!texture){
-        for(var key in this.textures){
-            if(this.textures[key].url === name){
-                texture = this.textures[key];
+AssetCache.prototype.addAudio = function(name, url, audio){
+    return this._addAssetIn(this.audios, name, url, audio);
+};
+
+AssetCache.prototype.getAudio = function(name){
+    return this._getAssetFrom(this.audios, name);
+};
+
+AssetCache.prototype._addAssetIn = function(list, name, url, asset){
+    name = name || url;
+    list[name] = {
+        url: url,
+        asset: asset
+    };
+
+    return this;
+};
+
+AssetCache.prototype._getAssetFrom = function(list, name){
+    var asset = list[name];
+
+    if(!asset){
+        for(var key in list){
+            if(list[key].url === name){
+                asset = list[key];
                 break;
             }
         }
     }
 
-    return (texture) ? texture.asset : null;
+    return (asset) ? asset.asset : null;
 };
+
+
 
 module.exports = AssetCache;
