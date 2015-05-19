b41b8ba1a170fa160c2a95b89a32190eb5af8ab8
Nazariglez <nazari.nz@gmail.com>
2015-05-19 21:04:13 +0100

    some changes in audio manager and audio object

--- src/audio/Audio.js
+++ src/audio/Audio.js
@@ -1,8 +1,50 @@
-function Audio(){
-    this._init();
+function Audio(manager, data, id){
+    this._init(manager, data, id);
 }
 
 Audio.prototype.constructor = Audio;
+
+Audio.prototype._init = function(manager, data, id){
+    this.manager = manager;
+    this.id = id;
+    this.context = this.manager.context;
+    this.source = null;
+
+    this.loop = false;
+
+    if(this.context){
+        this.gainNode = this.context.createGain ? this.context.createGain() : this.context.createGainNode();
+        this.context.decodeAudioData(data, this._decoded.bind(this));
+    }else{
+        this.source = data;
+    }
+};
+
+Audio.prototype._decoded = function(buffer){
+    this.source = this.context.createBufferSource();
+    if (!this.source.start)this.source.start = this.source.noteOn;
+    if (!this.source.stop)this.source.stop = this.source.noteOff;
+
+    this.source.buffer = buffer;
+    this.source.loop = false;
+
+    this.source.onended = this._onEnd.bind(this);
+};
+
+Audio.prototype.play = function(){
+    if(this.context) {
+        this.gainNode.gain.value = 1;
+        this.gainNode.connect(this.manager.gainNode);
+        this.source.connect(this.gainNode);
+
+        this.source.start(0, 0);
+    }
+
+};
+
+Audio.prototype._onEnd = function(){
+    console.log(this.id, 'end');
+};
 
 module.exports = Audio;
 
--- src/audio/AudioManager.js
+++ src/audio/AudioManager.js
@@ -1,3 +1,7 @@
+var Device = require('../core/Device'),
+    Audio = require('./Audio'),
+    utils = require('../core/utils');
+
 function AudioManager(game){
     this._init(game);
 }
@@ -7,10 +11,23 @@ AudioManager.prototype.constructor = AudioManager;
 AudioManager.prototype._init = function(game){
     this.game = game;
     this.audios = [];
+    this.context = null;
+
+    if(this.game.isWebAudio){
+        this.context = new Device.webAudioContext();
+        this.gainNode = this.context.createGain ? this.context.createGain() : this.context.createGainNode();
+        this.gainNode.connect(this.context.destination);
+    }
 };
 
 AudioManager.prototype.add = function(audio){
     this.audios.push(audio);
+    return this;
+};
+
+AudioManager.prototype._decodeAudio = function(name, url, data){
+    name = name || url;
+    utils.assetCache.addAudio(name, url, new Audio(this, data, name));
     return this;
 };
 
