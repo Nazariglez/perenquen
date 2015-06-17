a075d7411ed5f79f7581a7950069c4f71c71b33b
Nazariglez <nazari.nz@gmail.com>
2015-06-18 00:43:19 +0100

    added a gamepad callback system

--- src/input/gamepad/Controller.js
+++ src/input/gamepad/Controller.js
@@ -1,3 +1,5 @@
+var GamepadButton = require('./GamepadButton');
+
 function Controller(id, gamepadManager){
     this._init(id, gamepadManager);
 }
@@ -9,11 +11,21 @@ Controller.prototype._init = function(id, gamepadManager){
     this.manager = gamepadManager;
     this.axeSensibility = this.manager.axeSensibility;
 
+    this.pressedButtons = [];
+    this.releasedButtons = [];
+    this.downButtons = [];
+
     this.isConnected = false;
     this.axes = [];
     this.buttons = [];
     this.type = "";
     this.lastTime = 0;
+
+    this.callbacks = {
+        down : [],
+        pressed : [],
+        released : []
+    };
 };
 
 Controller.prototype.connect = function(data){
@@ -33,7 +45,6 @@ Controller.prototype.disconnect = function(){
 };
 
 Controller.prototype.updateData = function(data){
-    if(data.timestamp === this.lastTime)return this;
     this.lastTime = data.timestamp;
 
     var i;
@@ -44,6 +55,31 @@ Controller.prototype.updateData = function(data){
             this.axes[i] = 0;
         }
     }
+
+    for(i = 0; i < data.buttons.length; i++){
+        var button = data.buttons[i];
+        this.pressedButtons[i] = false;
+        this.releasedButtons[i] = false;
+
+
+        if(button.pressed){
+            if(!this.downButtons[i]){
+                this.pressedButtons[i] = true;
+                this.downButtons[i] = true;
+                this._callback(this.callbacks.pressed, i);
+            }
+
+            this._callback(this.callbacks.down, i);
+        }
+
+        if(!button.pressed && this.downButtons[i]){
+            this.releasedButtons[i] = true;
+            this.downButtons[i] = false;
+            this.pressedButtons[i] = false;
+            this._callback(this.callbacks.released, i);
+        }
+
+    }
     this.buttons = data.buttons;
 
 };
@@ -53,6 +89,51 @@ Controller.prototype._reset = function(){
     this.buttons = [];
     this.type = "";
     this.lastTime = 0;
+};
+
+Controller.prototype.isPressed = function(key){
+    return !!this.pressedButtons[key];
+};
+
+Controller.prototype.isReleased = function(key){
+    return !!this.releasedButtons[key];
+};
+
+Controller.prototype.isDown = function(key){
+    return !!this.downButtons[key];
+};
+
+Controller.prototype.addCallback = function(state, cb){
+    if(state !== "down" && state !== "pressed" && state !== "released"){
+        throw new Error('Wrong keyboard state name');
+    }
+
+    var index = this.callbacks[state].indexOf(cb);
+    if(index === -1){
+        this.callbacks[state].push(cb);
+    }
+
+    return this;
+};
+
+Controller.prototype.removeCallback = function(state, cb){
+    if(state !== "down" && state !== "pressed" && state !== "released"){
+        throw new Error('Wrong keyboard state name');
+    }
+
+    var index = this.callbacks[state].indexOf(cb);
+    if(index >= 0){
+        this.callbacks[state].splice(index, 1);
+    }
+
+    return this;
+};
+
+Controller.prototype._callback = function(callbacks, key){
+    var len = callbacks.length;
+    for(var i = 0; i < len; i++){
+        callbacks[i](key);
+    }
 };
 
 module.exports = Controller;
---
+++ src/input/gamepad/GamepadButton.js
@@ -0,0 +1,12 @@
+function GamepadButton(){
+    this._init();
+}
+
+GamepadButton.prototype.constructor = GamepadButton;
+
+GamepadButton.prototype._init = function(){
+
+};
+
+module.exports = GamepadButton;
+
